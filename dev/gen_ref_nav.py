e2c689a102624ac1e203a2b511b2ebd792048710
FlorianWilhelm <FlorianWilhelm@users.noreply.github.com>
2022-11-06 19:37:08 +0000

    Activate also vcr unit tests for github workflow 3c20b9002fe497ecb9c56de4d53e0c3d57df5d36

--- dev/gen_ref_nav.py
+++ dev/gen_ref_nav.py
@@ -24,6 +24,8 @@ for path in sorted(Path("src").rglob("*.py")):
         full_doc_path = full_doc_path.with_name("index.md")
     elif parts[-1] == "__main__":
         continue
+    elif parts[-1] == "_version":
+        continue
 
     nav[parts] = doc_path.as_posix()
 
