ee399f73462c7112c9925746e5c9791a7451affd
Florian Wilhelm <florian.wilhelm@gmail.com>
2023-11-26 09:04:06 +0100

    Fix the unit tests on windows

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,6 @@
 """One test for each example in the examples folder"""
 
+import os
 import re
 import subprocess
 
@@ -8,6 +9,7 @@ def test_getting_started():
     with open('examples/getting_started.py', encoding='utf8') as file:
         py_file = file.read()
 
+    py_file = py_file.replace('\n', os.linesep)  # for windows
     py_file = re.sub(r'auth=TOKEN', '', py_file, flags=re.MULTILINE)
     # avoid the fact that we open up another ultimate-notion client
     result = subprocess.run(['python', '-c', py_file], capture_output=True, text=True, check=True)  # noqa: S603, S607
