bbd9e51bddcf53e53ef575a66646018d5012518d
Florian Wilhelm <florian.wilhelm@gmail.com>
2023-12-03 14:30:15 +0100

    Skip doc unittest on windows

--- tests/test_docs_py_snippets.py
+++ tests/test_docs_py_snippets.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import sys
+
+import pytest
 from mktestdocs import check_md_file
 
 
+@pytest.mark.skipif(sys.platform == 'win32', reason="UnicodeDecodeError: 'charmap' codec can't decode")
 def test_db_introduction():
     check_md_file(fpath='docs/usage/db_introduction.md', memory=True)
