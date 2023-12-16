032d09782b4016d24f81f906faed71488ff40267
FlorianWilhelm <FlorianWilhelm@users.noreply.github.com>
2023-12-16 21:26:35 +0000

    Fix broken link 10f793a3ddde53562d6ed1da3f323f2402e497b7

--- dev/scripts/gen_ref_nav.py
+++ dev/scripts/gen_ref_nav.py
@@ -21,6 +21,8 @@ for path in sorted(Path('src').glob('**/*.py')):
         parts = parts[:-1]
         doc_path = doc_path.with_name('index.md')
         full_doc_path = full_doc_path.with_name('index.md')
+    elif parts[-1] == '_version':
+        continue
     elif parts[-1] == '__main__':
         continue
     nav_parts = list(parts)
