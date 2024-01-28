5f2ea170097229de7f9afc798f32f1b8b4f7d445
Florian Wilhelm <florian.wilhelm@gmail.com>
2024-01-28 20:51:27 +0100

    Most markdown elements work

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -38,10 +38,23 @@ def test_multi_select_options(all_cols_db: Database):
 
 @pytest.mark.vcr()
 def test_rich_text_md(md_page: Page):
+    """These markdowns were tested with https://stackedit.io/app#"""
     correct_mds = [
+        'here is something **very** *simpel* and <u>underlined</u> as well as `code`',
         '**here is a sentence that was bolded *then* typed.**',
         'here is a test sentence with ~~many **different *styles*.**~~',
         'here is another test with ~~many *different **styles**.*~~',
+        'here is one more with a *strange **style*** **combination**',
+        'here is one with an inline **~~equa-*tion*~~ *$E=mc^2$ and*** no block equation',
+        (
+            'and here is one with ~~***person** mention [@Florian Wilhelm]()*~~ and **page mention '
+            '↗[Markdown Test](https://www.notion.so/0c8ea7f1c7ca4abb8890085c0fac383b)** '
+        ),
+        'here is one **stretching over *many\n~~many~~*\n~~lines~~**',
+        # ToDo: This is not 100% right as code blocks with line breaks are not supported in Markdown.
+        # Thus '\n' in the last code block will be ignored and the code will be in one line.
+        # To Fix this we would have to break rich_texts into multiple code blocks.
+        'This is code, e.g. **`python` code\nnow stretching `over\nmany lines`**',
     ]
     for idx, block in enumerate(md_page.content):
         assert isinstance(block, TextBlock)
