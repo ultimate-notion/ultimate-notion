f3bede9842e717d76e470dc453c22bb80612ddd3
Florian Wilhelm <florian.wilhelm@gmail.com>
2024-05-23 13:58:43 +0200

    Fix nested asyncio.run problem by going back to vcrpy 5.0.1

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,10 +1,8 @@
 """One test for each example in the examples folder"""
 
-import asyncio
 import contextlib
 import shutil
 import sys
-from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 import pytest
@@ -19,39 +17,10 @@ def notion_cleanups():
     """Overwrites fixture from conftest.py to avoid an open session."""
 
 
-def run_in_new_loop(code, file_path):
-    """Function to run provided code in a new event loop in a separate thread."""
-
-    async def async_exec(compiled_code):  # noqa: RUF029
-        # Create a new event loop for this thread
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        try:
-            # Execute the compiled code in the new loop
-            exec(compiled_code, {'__MODULE__': '__main__', 'asyncio': asyncio})  # noqa: S102
-        finally:
-            # Close the loop after execution
-            loop.close()
-
-    # Compile the code for execution
-    compiled_code = compile(code, file_path, 'exec')
-
-    # Run the async_exec coroutine in the new event loop
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(async_exec(compiled_code))
-    finally:
-        loop.close()
-
-
 def exec_pyfile(file_path: str) -> None:
-    """Executes a Python module as a script (like CLI) in a separate thread with its own event loop."""
-    code = Path(file_path).read_text(encoding='utf-8')
-
-    # Execute in a separate thread with its own event loop
-    with ThreadPoolExecutor() as executor:
-        executor.submit(run_in_new_loop, code, file_path)
+    """Executes a Python module as a script, as if it was called from the command line."""
+    code = compile(Path(file_path).read_text(encoding='utf-8'), file_path, 'exec')
+    exec(code, {'__MODULE__': '__main__'})  # noqa: S102
 
 
 @pytest.mark.vcr()
