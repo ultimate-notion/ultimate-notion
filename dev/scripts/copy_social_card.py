5df9bc50ee5ea04cf1b3af5f76ebd9609c0ef502
FlorianWilhelm <FlorianWilhelm@users.noreply.github.com>
2024-08-07 15:06:17 +0000

    Fix social card e835dea919ad52534da65d9340dce20881cb8e0e

--- dev/scripts/copy_social_card.py
+++ dev/scripts/copy_social_card.py
@@ -1,9 +1,18 @@
 """Override the default social card image as we are using a custom image for the social card."""
 
+import logging
 import os
 import shutil
+import sys
+
+_logger = logging.getLogger(__name__)
+
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s    -  %(message)s', stream=sys.stdout)
 
 
 def on_post_build(config, **kwargs):
-    if os.path.exists('assets/images/social'):  # only on gh-pages branch
-        shutil.copyfile('docs/assets/images/social-card.png', 'assets/images/social/index.png')
+    _logger.info('Running post build actions...')
+    if os.path.exists('site/assets/images/social'):  # only on gh-pages branch
+        _logger.info('Copying custom social card...')
+        shutil.copyfile('docs/assets/images/social-card.png', 'site/assets/images/social/index.png')
