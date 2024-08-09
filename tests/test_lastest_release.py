568583091f82bc88cdcb822c6bcc07bd910f1474
Florian Wilhelm <florian.wilhelm@gmail.com>
2024-08-09 15:24:46 +0200

    Add workflow to test the latest release periodically

--- tests/test_lastest_release.py
+++
@@ -1,8 +0,0 @@
-"""This test module only tests the latest release of Ultimate-Notion.
-
-This is to ensure that the latest release is working as expected as the Notion API is subject to frequent changes.
-"""
-
-from __future__ import annotations
-
-# ToDo: Implement
