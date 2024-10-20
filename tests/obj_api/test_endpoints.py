3cee8cff09fb333c50e24fa5b542bd716ded1843
Florian Wilhelm <florian.wilhelm@gmail.com>
2024-10-20 17:47:17 +0200

    Finalize page properties retrieval endpoint by adding all possible property items

---
+++ tests/obj_api/test_endpoints.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import pytest
+
+import ultimate_notion as uno
+
+
+@pytest.mark.vcr()
+def test_retrieve_property(all_props_db: uno.Database):
+    page = all_props_db.fetch_all().to_pages()[0]
+    page_obj = page.obj_ref
+    page_props = page.props._prop_vals
+    notion = uno.Session.get_or_create()
+
+    for _, prop_val in page_props.items():
+        list(notion.api.pages.properties.retrieve(page_obj, prop_val))
