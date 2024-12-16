268012c2b1a27feeb6a28a57d7183f90d773b983
Florian Wilhelm <florian.wilhelm@gmail.com>
2024-12-16 12:24:13 +0100

    Added missing return type hints for all tests

--- tests/test_option.py
+++ tests/test_option.py
@@ -9,7 +9,7 @@ from ultimate_notion.schema import MultiSelect, Select, Status
 
 
 @pytest.mark.vcr()
-def test_status_options_groups(all_props_db: uno.Database):
+def test_status_options_groups(all_props_db: uno.Database) -> None:
     status_prop_type = cast(Status, all_props_db.schema.get_prop('Status').type)
     all_options = ['Not started', 'In progress', 'Done']
     assert [option.name for option in status_prop_type.options] == all_options
@@ -22,14 +22,14 @@ def test_status_options_groups(all_props_db: uno.Database):
 
 
 @pytest.mark.vcr()
-def test_select_options(all_props_db: uno.Database):
+def test_select_options(all_props_db: uno.Database) -> None:
     select_prop_type = cast(Select, all_props_db.schema.get_prop('Select').type)
     all_options = ['Option1', 'Option2']
     assert [option.name for _, option in select_prop_type.options.items()] == all_options
 
 
 @pytest.mark.vcr()
-def test_multi_select_options(all_props_db: uno.Database):
+def test_multi_select_options(all_props_db: uno.Database) -> None:
     multi_select_prop_type = cast(MultiSelect, all_props_db.schema.get_prop('Multi-Select').type)
     all_options = ['MultiOption1', 'MultiOption2']
     assert [option.name for _, option in multi_select_prop_type.options.items()] == all_options
